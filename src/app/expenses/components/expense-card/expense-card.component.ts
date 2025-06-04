7b6af17d05be2451f75f525ec73742b65393ee00
Erick130601 <erickruiztorres13062001@gmail.com>
2025-06-04 14:38:04 -0500

    for fix: change in expense_ocultar

--- src/app/expenses/components/expense-card/expense-card.component.ts
+++ src/app/expenses/components/expense-card/expense-card.component.ts
@@ -4,6 +4,13 @@ import { ExpensesEntity } from '../../model/expenses.entity';
 import { ContactService } from "../../../contacts/services/contact.service";
 import { ExpensesService } from '../../services/expenses.service';
 
+
+export interface Expense {
+  id: number;
+  title: string;
+  description: string;
+  amount: number;
+}
 @Component({
   selector: 'app-expense-card',
   templateUrl: './expense-card.component.html',
@@ -11,9 +18,17 @@ import { ExpensesService } from '../../services/expenses.service';
 })
 export class ExpenseCardComponent implements OnInit {
   @Input() expense: ExpensesEntity = new ExpensesEntity();
+  @Input() isFirst: boolean = false;
   @Output() expenseDeleted: EventEmitter<number> = new EventEmitter<number>();
   user: any;
 
+  // Visibilidad individual
+
+
+  // Solo una instancia muestra el botón global
+  static showToggleButtonRendered = false;
+  showToggleButton = false;
+  visibleCards: { [key: number]: boolean } = {};
   constructor(
     private userService: ContactService,
     private expensesService: ExpensesService
@@ -26,13 +41,28 @@ export class ExpenseCardComponent implements OnInit {
         console.log('User loaded:', this.user);
       });
     }
+    // Mostrar todos los cards al cargar
+    if (this.expense && this.expense.id) {
+      this.visibleCards[this.expense.id] = true;
+    }
+  }
+  // Oculta un card por ID
+  hideCard(id: number): void {
+    this.visibleCards[id] = false;
+  }
+
+  // Muestra todos los cards
+  showAllCards(): void {
+    for (const id in this.visibleCards) {
+      this.visibleCards[id] = true;
+    }
   }
 
   deleteExpense(expenseId: number): void {
     this.expensesService.deleteExpenseById(expenseId).subscribe(
       () => {
         console.log(`Expense with ID ${expenseId} deleted successfully.`);
-        this.expenseDeleted.emit(expenseId); 
+        this.expenseDeleted.emit(expenseId);
       },
       (error) => {
         console.error('Error deleting expense:', error);
